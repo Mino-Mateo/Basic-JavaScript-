bb8c6c9ce7cc70248f05e092b6601dccbf088489
Mateo Miño <minora.mateo@gmail.com>
2025-01-17 18:10:21 -0500

    Introducción a JavaScript

---
+++ 00-Hello.js
@@ -0,0 +1,21 @@
+/*
+Comentarios en 
+varias lineas
+*/
+
+// Comentario en una línea
+
+// Texto
+console.log("Hola Mundo");
+console.log(`Buenas noches`);
+console.log("Buenos dias");
+console.log(5);
+console.log("5");
+
+// Tipos de Datos
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 + 2);
+console.log(5 ** 2);
+console.log(5 % 2);
