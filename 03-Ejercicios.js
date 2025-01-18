87dde3b3be0dc39b486afe61634a7d0c00902ce4
Mateo Miño <minora.mateo@gmail.com>
2025-01-17 19:44:54 -0500

    Ejercicios

---
+++ 03-Ejercicios.js
@@ -0,0 +1,118 @@
+// 1. Escribe un comentario en una línea
+// COMO ASI?????
+
+// 2. Escribe un comentario en varias líneas
+/*
+OSEA
+ASI
+??????
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let numero = 5;
+let decimal = 342.96;
+let palabra = "Recorcholis";
+let verdadero = false;
+let grandote = 123456789123456789123465789123456789n;
+let simbolico = Symbol("😈");
+let definidoNot;
+let nulidad = null;
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(numero);
+console.log(decimal);
+console.log(palabra);
+console.log(verdadero);
+console.log(grandote);
+console.log(simbolico);
+console.log(definidoNot);
+console.log(nulidad);
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof numero);
+console.log(typeof decimal);
+console.log(typeof palabra);
+console.log(typeof verdadero);
+console.log(typeof grandote);
+console.log(typeof simbolico);
+console.log(typeof definidoNot);
+console.log(typeof nulidad);
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+numero = 6;
+decimal = 65.12;
+palabra = "Carambanos";
+verdadero = true;
+grandote = 1323121346546512809890213684798798465465516n;
+simbolico = Symbol("😇");
+definidoNot != undefined;
+nulidad != null;
+
+console.log(numero);
+console.log(decimal);
+console.log(palabra);
+console.log(verdadero);
+console.log(grandote);
+console.log(simbolico);
+console.log(definidoNot);
+console.log(nulidad);
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+numero = "Numerito bonito y precioso";
+decimal = true;
+palabra = 13231213465465128098902136847987984654655168123872189n;
+verdadero = true;
+grandote = Symbol("😇");
+simbolico = 3.14;
+definidoNot = null;
+nulidad = undefined;
+
+console.log(typeof numero);
+console.log(typeof decimal);
+console.log(typeof palabra);
+console.log(typeof verdadero);
+console.log(typeof grandote);
+console.log(typeof simbolico);
+console.log(typeof definidoNot);
+console.log(typeof nulidad);
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const eternoNumero = 12345;
+const eternoDecimal = 52.68;
+const eternaPalabra = "Ouroboros";
+const eternoVerdadero = false;
+const eternoGrandote = 45465468465481798798818418n;
+const eternoSimbolico = Symbol("♾️");
+const eternoDefinidoNot = undefined;
+const eternaNulidad = null;
+
+console.log(eternoNumero);
+console.log(eternoDecimal);
+console.log(eternaPalabra);
+console.log(eternoVerdadero);
+console.log(eternoGrandote);
+console.log(eternoSimbolico);
+console.log(eternoDefinidoNot);
+console.log(eternaNulidad);
+
+// 9. A continuación, modifica los valores de las constantes
+
+// eternoNumero = 123468445;
+// eternoDecimal = 78.411;
+// eternaPalabra = "Aeternum";
+// eternoVerdadero = true;
+// eternoGrandote = 84981659841654213213222121321n;
+// eternoSimbolico = Symbol("🌡");
+// eternoDefinidoNot = undefined;
+// eternaNulidad = null;
+
+console.log(eternoNumero);
+console.log(eternoDecimal);
+console.log(eternaPalabra);
+console.log(eternoVerdadero);
+console.log(eternoGrandote);
+console.log(eternoSimbolico);
+console.log(eternoDefinidoNot);
+console.log(eternaNulidad);
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
